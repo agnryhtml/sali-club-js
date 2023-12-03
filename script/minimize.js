efab1117a64bb9cc5a146639597918f75e4c23f7
angryhtml <kolesslilidesign@gmail.com>
2023-12-03 21:58:11 +0100

    js for opening articles

--- script/minimize.js
+++ script/minimize.js
@@ -1,21 +1,8 @@
 let blogArticles = document.querySelectorAll('.blog-article.short');
 
 for (let blogArticle of blogArticles) {
-  let moreButton = document.querySelector('.more');
+  let moreButton = blogArticle.querySelector('.more');
   moreButton.onclick = function () {
-  blogArticle.classList.remove('short');
+   blogArticle.classList.remove('short');
   };
 }
-
-// let blogArticleOne = document.querySelector('.blog-article-1.short');
-// let blogArticleTwo = document.querySelector('.blog-article-2.short');
-// let moreButton = document.querySelector('.more');
-
-// moreButton.onclick = function () {
-//   blogArticleOne.classList.remove('short');
-//   };
-
-// moreButton.onclick = function () {
-//   blogArticleTwo.classList.remove('short');
-// };
-  
